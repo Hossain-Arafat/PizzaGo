d99686d5d44d8e15dc45e811f15163952a78b877
Arafat <ar.hossain1303@gmail.com>
2026-01-20 04:00:04 +0600

    Design update of Change Password page

--- view/change_password.php
+++ view/change_password.php
@@ -14,29 +14,32 @@ $emailValue = $_SESSION['email'] ?? "";
     <title>Change Password - PizzaGo</title>
     <link rel="shortcut icon" href="../image/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/common.css">
-
     <link rel="stylesheet" href="../css/change_password.css">
 </head>
 <body>
 
-<h1>Change Password</h1>
-<div class="box">
-    
+<?php include "header.php"; ?>
 
-    <form action="../controller/passwordController.php" method="post" novalidate>
-        <label>Email</label>
-        <input type="email" name="email" value="<?= htmlspecialchars($emailValue) ?>" readonly>
+<div class="page">
+    <div class="card">
+        <h1 class="title">Change Password</h1>
 
-        <label>New Password *</label>
-        <input type="password" name="password" placeholder="Enter new password">
+        <form action="../controller/passwordController.php" method="post" novalidate>
+            <label>Email</label>
+            <input type="email" name="email" value="<?= htmlspecialchars($emailValue) ?>" readonly>
 
-        <div class="buttons">
-            <button type="submit" class="primary">Save Changes</button>
-            <a href="profile.php" class="cancel">Cancel</a>
-        </div>
-    </form>
+            <label>New Password <span class="req">*</span></label>
+            <input type="password" name="password" placeholder="Enter new password">
+
+            <div class="buttons">
+                <button type="submit" class="primary">Save Changes</button>
+                <a href="profile.php" class="cancel">Cancel</a>
+            </div>
+        </form>
+    </div>
 </div>
-<?php include "footer.php" ?>
+
+<?php include "footer.php"; ?>
 
 </body>
 </html>
